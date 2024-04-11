0d246c2cab92dd3eb484186d7d5503247ba938ab
Sammy Lee <145081077+leesemin89@users.noreply.github.com>
2024-04-11 15:24:01 -0500

    함수 표현식

--- _posts/Javascript/examples.js
+++ _posts/Javascript/examples.js
@@ -1,5 +1,12 @@
-let isChecked = false;
-let isAdmin = true;
-let isVisiable = true;
-let isOnline = false;
-let hasAc
+function outerFunction(x){
+    return function innerFunction(y){
+        return x + y;
+    };
+}
+let add5 = outerFunction(5);
+console.log(add5(3)); // output: 8
+console.log(add5(10)); // 15
+
+let add10 = outerFunction(10);
+console.log(add10(3)); // 13
+console.log(add10(10)); // 20
