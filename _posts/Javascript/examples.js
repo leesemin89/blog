a0d5cc2920d9a30c3fb83a432e401f54375e794a
Sammy Lee <145081077+leesemin89@users.noreply.github.com>
2024-04-11 21:16:09 -0500

    명명된 인수

--- _posts/Javascript/examples.js
+++ _posts/Javascript/examples.js
@@ -1,12 +1,9 @@
-function outerFunction(x){
-    return function innerFunction(y){
-        return x + y;
-    };
-}
-let add5 = outerFunction(5);
-console.log(add5(3)); // output: 8
-console.log(add5(10)); // 15
+let greetUser = (name) => {
+    console.log("Hello " + name + "!");
+};
 
-let add10 = outerFunction(10);
-console.log(add10(3)); // 13
-console.log(add10(10)); // 20
+greetUser("John");
+greetUser("Jane");
+
+let userName = "Bob";
+greetUser(userName);
