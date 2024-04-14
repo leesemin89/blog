604ecda4f640ffcb377f479b9dce837e73e26fdd
Sammy Lee <145081077+leesemin89@users.noreply.github.com>
2024-04-14 00:43:24 -0500

    blog

--- _posts/Javascript/examples.js
+++ _posts/Javascript/examples.js
@@ -1,9 +1,13 @@
-let greetUser = (name) => {
-    console.log("Hello " + name + "!");
-};
+let age = 25;
+let citizenship = "Non-US";
 
-greetUser("John");
-greetUser("Jane");
-
-let userName = "Bob";
-greetUser(userName);
+if(age >= 18 && (citizenship === "US" || citizenship === "EU")){
+    console.log("Eligible for the program");
+} else{
+    console.log("Not Eligible for the program");
+}
+if (!(age < 18)){
+    console.log("Age is 18 or older");
+} else {
+    console.log("Age is less than 18");
+}
