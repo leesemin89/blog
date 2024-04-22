2b3739e822931f4ded4329dc0b68f99e8cccbd51
Sammy Lee <145081077+leesemin89@users.noreply.github.com>
2024-04-21 23:11:15 -0500

    배열

--- _posts/Javascript/examples.js
+++ _posts/Javascript/examples.js
@@ -1,11 +1,18 @@
-var name = "John";
-var age = 30;
-var address = "USA";
-
-function someFunction(){
-    var name = "Jane";
-    console.log(name); // logs "Jane" to console.
+const numbers = {
+    *[Symbol.iterator](){
+        for(let i = 1; i <= 10; i++){
+            yield i;
+        }
+    }
+};
+const square = x => x * x;
+const mappedNumbers = {
+    *[Symbol.iterator](){
+        for(const number of numbers){
+            yield square(number);
+        }
+    }
+};
+for(const mappedNumber of mappedNumbers){
+    console.log(mappedNumber);
 }
-
-someFunction();
-console.log(name);
